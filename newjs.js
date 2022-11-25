c17f9135ec881924ded9834422c7d0fba0b96942
Neeraj-Joshi4454 <neerajmjoshi1@gmail.com>
2022-11-25 16:49:42 +0530

    first commit

---
+++ newjs.js
@@ -0,0 +1,14 @@
+
+
+ function loadText(){
+            
+    // document.getElementsByClassName('heading1')[0].innerHTML="Hello from javascript";
+    // document.getElementsByClassName('heading1')[4].innerHTML="ABCs"
+
+
+    // document.getElementById('heading5').innerHTML=Date();
+
+
+    // window.alert('Hello from js and welcome to our site')
+
+}
